90892fcfd7d25355d3c18cd337980d7ce490e6a8
jianghouwei <jianghouwei@haolianluo.com>
2016-04-01 10:17:02 +0800

    new

--- src/main/java/com/tianyu/jty/shop/dao/AdverDao.java
+++ src/main/java/com/tianyu/jty/shop/dao/AdverDao.java
@@ -1,5 +1,7 @@
 package com.tianyu.jty.shop.dao;
 
+import org.springframework.stereotype.Repository;
+
 import com.tianyu.jty.common.persistence.HibernateDao;
 import com.tianyu.jty.shop.entity.Adver;
 
@@ -8,6 +10,7 @@ import com.tianyu.jty.shop.entity.Adver;
  * @author ty
  * @date 2015年1月22日
  */
+@Repository
 public class AdverDao  extends HibernateDao<Adver, Integer>{
 
 }
